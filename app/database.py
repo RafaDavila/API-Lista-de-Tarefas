cdd11147d34bfca069758ce78abf22fdefd2e856
Rafael Davila <rafaeldavila924@gmail.com>
2026-07-27 20:08:51 -0300

    Configura variáveis de ambiente e migração para o PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,13 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
